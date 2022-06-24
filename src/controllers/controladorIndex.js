254c280ab4f44253b87e65c24f1e79ae550ccbf1
308 <308@academicos.cesde.edu.co>
2022-06-24 12:39:21 -0500

    se crea interfaz index

---
+++ src/controllers/controladorIndex.js
@@ -0,0 +1,14 @@
+
+
+//controlando etiquetas
+
+//1.necesito almacenar una etiquetar en una variable
+let titulo=document.getElementById("titulo")
+let imagen=document.getElementById("foto")
+
+//2. identifico que quiero controlar
+//2.1 cambiando el texto
+titulo.textContent="chao"
+
+//2.2 cambiando la fuente src
+imagen.src="https://placeimg.com/640/480/any"
